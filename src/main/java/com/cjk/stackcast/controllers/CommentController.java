e8218518318b09955f76fb33b84d5077c434dbc8
jchuru <jameschuru123@gmail.com>
2020-04-06 15:51:34 -0400

    --

--- src/main/java/com/cjk/stackcast/controllers/CommentController.java
+++ src/main/java/com/cjk/stackcast/controllers/CommentController.java
@@ -4,8 +4,12 @@ import com.cjk.stackcast.models.comment.Comment;
 import com.cjk.stackcast.services.CommentService;
 import org.hibernate.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping( value = "zc-video-app/comments")
@@ -13,6 +17,26 @@ public class CommentController {
 
     @Autowired
     private CommentService service;
+
+
+    @PostMapping("/create")
+    public ResponseEntity<Comment> create(@RequestBody Comment comment){
+
+        Comment newComment = this.service.create(comment);
+        try {
+            return ResponseEntity
+                    .created( new URI("/create" + newComment.getCommentId()))
+                    .body(newComment);
+        } catch (URISyntaxException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+
+    }
+
+    @GetMapping("/show")
+    public ResponseEntity<Iterable<Comment>> showAll(){
+        return new ResponseEntity<>(service.showAll() , HttpStatus.OK);
+    }
 
     @GetMapping
     public String findComentsByVideoId(@RequestParam Long videoId){
--- src/main/java/com/cjk/stackcast/services/CommentService.java
+++ src/main/java/com/cjk/stackcast/services/CommentService.java
@@ -9,10 +9,18 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
+
 public class CommentService {
 
     @Autowired
     private CommentRepository comentRopo;
+
+    public Comment create(Comment comment){
+        return comentRopo.save(comment);
+    }
+    public Iterable<Comment> showAll(){
+        return comentRopo.findAll();
+    }
 
 
 

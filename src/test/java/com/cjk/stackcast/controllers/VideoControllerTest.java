9b7cad4ef6a9c3fb9ef515b4a04c4ef963b207f6
k-romero <imkevinromero@gmail.com>
2020-04-11 14:35:02 -0400

    create updateVideoPath and updateUser controller tests

--- src/test/java/com/cjk/stackcast/controllers/VideoControllerTest.java
+++ src/test/java/com/cjk/stackcast/controllers/VideoControllerTest.java
@@ -1,7 +1,9 @@
 package com.cjk.stackcast.controllers;
 
+import com.cjk.stackcast.models.User;
 import com.cjk.stackcast.models.Video;
 import com.cjk.stackcast.services.VideoService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,9 +15,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -72,12 +76,16 @@ public class VideoControllerTest {
     void testUpdateName() throws Exception{
         //Given
         String newName = "newVideoName";
+        Video mockVideo = new Video(1L,"Test Videos","https://testPath.com/test","video/mp4");
         Video putVideo = new Video(1L,newName,"https://testPath.com/test","video/mp4");
+        doReturn(Optional.of(mockVideo)).when(videoService).show(1L);
         doReturn(putVideo).when(videoService).updateVideoName(1L,newName);
 
         //Execute
         mockMvc.perform(put("/zc-video-app/videos/updateName/{id}",1)
-                            .param("videoName",newName))
+                            .param("videoName",newName)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(asJsonString(putVideo)))
 
                             //Validate
                             .andExpect(status().isOk())
@@ -87,18 +95,73 @@ public class VideoControllerTest {
     @Test
     @DisplayName("PUT /videos/incrementViews/{id}")
     void testIncrementViews() throws Exception{
-
-        //TODO write actual test
         //Given
-        Integer newViewCount = 0;
+        Integer newViewCount = 1;
+        Video mockVideo = new Video(1L,"Test Videos","https://testPath.com/test","video/mp4");
         Video putVideo = new Video(1L,"Test Videos","https://testPath.com/test","video/mp4");
+        putVideo.setVideoViews(newViewCount);
+        doReturn(Optional.of(mockVideo)).when(videoService).show(1L);
         doReturn(putVideo).when(videoService).incrementVideoViews(1L);
 
         //Execute
-        mockMvc.perform(put("/zc-video-app/videos/incrementViews/{id}",1))
+        mockMvc.perform(put("/zc-video-app/videos/incrementViews/{id}",1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(putVideo)))
 
                 //Validate
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.videoViews",is(newViewCount)));
+    }
+
+    @Test
+    @DisplayName("PUT /videos/updatePath/{id}")
+    void testUpdatePath() throws Exception{
+        //Given
+        String newPath = "https://newPath.com/testing";
+        Video mockVideo = new Video(1L,"Test Videos","https://testPath.com/test","video/mp4");
+        Video putVideo = new Video(1L,"Test Videos",newPath,"video/mp4");
+        doReturn(Optional.of(mockVideo)).when(videoService).show(1L);
+        doReturn(putVideo).when(videoService).updateVideoPath(1L,newPath);
+
+        //Execute
+        mockMvc.perform(put("/zc-video-app/videos/updatePath/{id}",1)
+                .param("videoPath",newPath)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(putVideo)))
+
+                //Validate
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.videoPath",is(newPath)));
+    }
+
+    @Test
+    @DisplayName("PUT /videos/setUser/{id}")
+    void testSetUser() throws Exception{
+        //Given
+        Long user = 1L;
+        Video mockVideo = new Video(1L,"Test Videos","https://testPath.com/test","video/mp4");
+        Video putVideo = new Video(1L,"Test Videos","https://testPath.com/test","video/mp4");
+        putVideo.setUserId(user);
+        doReturn(Optional.of(mockVideo)).when(videoService).show(1L);
+        doReturn(putVideo).when(videoService).setUser(1L,user);
+
+        //Execute
+        mockMvc.perform(put("/zc-video-app/videos/setUser/{videoId}",1)
+                .param("userId", String.valueOf(user))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(putVideo)))
+
+                //Validate
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.userId",is(1)));
+    }
+
+
+    static String asJsonString(final Object obj){
+        try{
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
     }
 }

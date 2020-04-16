d6338fa914debfe9ce15e191a918adee22cc0238
k-romero <imkevinromero@gmail.com>
2020-04-16 16:07:56 -0400

    adding authorities

--- src/main/java/com/cjk/stackcast/security/SecurityConfiguration.java
+++ src/main/java/com/cjk/stackcast/security/SecurityConfiguration.java
@@ -12,15 +12,24 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    //THIS CLASS IS FOR LEARNING AND TESTING PURPOSES
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("admin").password(passwordEncoder().encode("admin123")).roles("ADMIN")
+                .withUser("admin")
+                .password(passwordEncoder().encode("admin123"))
+                .roles("ADMIN").authorities("ACCESS_TEST1, ACCESS_TEST2")
                 .and()
-                .withUser("kev").password(passwordEncoder().encode("kev123")).roles("USER")
+                .withUser("kev")
+                .password(passwordEncoder().encode("kev123"))
+                .roles("USER")
                 .and()
-                .withUser("manager").password(passwordEncoder().encode("manager123")).roles("MANAGER");
+                .withUser("manager")
+                .password(passwordEncoder().encode("manager123"))
+                .roles("MANAGER").authorities("ACCESS_TEST1");
     }
 
     @Override
@@ -28,9 +37,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/index.html").permitAll()
-                .antMatchers("/testProfile/index").authenticated()
-                .antMatchers("/admin/index").hasRole("ADMIN")
-                .antMatchers("/management/index").hasAnyRole("ADMIN","MANAGER")
+                .antMatchers("/testProfile/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/management/**").hasAnyRole("ADMIN","MANAGER")
+                .antMatchers("/zc-video-app/**").authenticated()
                 .and()
                 .httpBasic();
     }

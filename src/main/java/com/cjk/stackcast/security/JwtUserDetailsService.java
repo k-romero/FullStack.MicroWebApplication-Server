0707af93bf99a02d8c813131ba8907a4e5000e56
k-romero <imkevinromero@gmail.com>
2020-05-02 17:45:55 -0400

    putting JWT back in

--- src/main/java/com/cjk/stackcast/security/JwtUserDetailsService.java
+++ src/main/java/com/cjk/stackcast/security/JwtUserDetailsService.java
@@ -1,48 +1,48 @@
-//package com.cjk.stackcast.security;
-//
-//import com.cjk.stackcast.models.DAOUser;
-//import com.cjk.stackcast.models.UserDTO;
-//import com.cjk.stackcast.repositories.UserDaoRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.ArrayList;
-//import java.util.Optional;
-//
-//@Service
-//public class JwtUserDetailsService implements UserDetailsService {
-//
-//    @Autowired
-//    private UserDaoRepository userDaoRepository;
-//
-//    @Autowired
-//    private PasswordEncoder bcryptEncoder;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        Optional<DAOUser> user = userDaoRepository.findUserByUserName(username);
-//        if (!user.isPresent()) {
-//            throw new UsernameNotFoundException("User not found with username: " + username);
-//        } else {
-//            return new org.springframework.security.core.userdetails.User(user.get().getUserName(), user.get().getPassword(),
-//                    new ArrayList<>());
-//        }
-//    }
-//
-//    public boolean findUserByUserName(String name){
-//        return userDaoRepository.findUserByUserName(name).isPresent();
-//    }
-//
-//    public DAOUser save(UserDTO user) {
-//        System.out.println(user.toString());
-//        DAOUser newUser = new DAOUser();
-//        newUser.setUserName(user.getUserName());
-//        newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
-//        return userDaoRepository.save(newUser);
-//    }
-//
-//}
+package com.cjk.stackcast.security;
+
+import com.cjk.stackcast.models.DAOUser;
+import com.cjk.stackcast.models.UserDTO;
+import com.cjk.stackcast.repositories.UserDaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserDaoRepository userDaoRepository;
+
+    @Autowired
+    private PasswordEncoder bcryptEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<DAOUser> user = userDaoRepository.findUserByUserName(username);
+        if (!user.isPresent()) {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        } else {
+            return new org.springframework.security.core.userdetails.User(user.get().getUserName(), user.get().getPassword(),
+                    new ArrayList<>());
+        }
+    }
+
+    public boolean findUserByUserName(String name){
+        return userDaoRepository.findUserByUserName(name).isPresent();
+    }
+
+    public DAOUser save(UserDTO user) {
+        System.out.println(user.toString());
+        DAOUser newUser = new DAOUser();
+        newUser.setUserName(user.getUserName());
+        newUser.setPassword(bcryptEncoder.encode(user.getPassword()));
+        return userDaoRepository.save(newUser);
+    }
+
+}
